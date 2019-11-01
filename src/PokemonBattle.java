3bfeb93dc1ddce25b58f1dd06c549a8c3c7e95bc
schoeeva000 <schoeeva000@CAM-LAB-18.bgsd.k12.wa.us>
2019-11-01 07:49:57 -0700

    ya

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,34 @@
-
+/* PSUDOCODE
+ * 
+ * MAIN
+ * call battleStart, save the pokemon name return as a string
+ * call damage, passing in the name and saving the damage from the return
+ * call tableStats, passing in hp - damage
+ * 
+ * BATTLESTART
+ * Output an intro and prompt for the pokemon's name
+ * prompt for the rest of the stats and save them in variables
+ * call damage, passing in the name and saving the damage from the return
+ * 
+ * DAMAGE
+ * take in the stats from battlestart to use in the equation
+ * Modifier = STAB * math.random(between 0.85 and 1.0)
+ * Damage = (((2*level+10)/250 + (attack/defense)*base + 2)*modifier
+ * return the damage value
+ * call tableStats, passing in all the stats
+ * 
+ * TABLESTATS
+ * print out stats, having taken the new hp value
+ * 
+ */
 public class PokemonBattle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public static void battlestart() {
+		
 	}
 
 }
