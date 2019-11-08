2a93965aab9b8e126a74f9fb7a45048a4530ace7
wooki <wooki@HowlingPirates.Home>
2019-11-07 18:36:57 -0800

    Done

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -3,7 +3,7 @@
  * MAIN
  * call battleStart, save the pokemon name return as a string
  * call damage, passing in the name and saving the damage from the return
- * call tableStats, passing in hp - damage
+ * 
  * 
  * BATTLESTART
  * Output an intro and prompt for the pokemon's name
@@ -27,11 +27,16 @@ import java.util.*;
 public class PokemonBattle {
 
 	public static void main(String[] args) {
-		battlestart();
+		Scanner userJunk = new Scanner(System.in);
+
+		String name = battlestart(userJunk);
+		damage(name, userJunk);			//CHANGE back from double if only requires int
+		//I would normally call statsTable() from here, but I don't know how to
+		//return multiple parameters from damage.  Instead, I am calling it from damage,
+		// and passing in the stat parameters directly.
 	}
 	
-	public static void battlestart() {
-		Scanner userJunk = new Scanner(System.in);
+	public static String battlestart(Scanner userJunk) {
 		System.out.println("Welcome to Pokemon Battle! Type \"yes\" to continue...\n");
 		String useless = userJunk.nextLine(); 											//this part is really useless, just a way to separate the intro from the rest
 		
@@ -47,6 +52,47 @@ public class PokemonBattle {
 		
 		System.out.println("Zebstrika used a solid punch to knock out a molar!");
 		System.out.println("");
+		
+		return name;
+	}
+	
+	public static void damage(String name, Scanner userJunk) {
+		
+		//collect the stats for the pokemon
+		System.out.println("Trainer, what are " + name + "'s stats?");
+		System.out.print("Level: ");
+		int level = userJunk.nextInt();
+		System.out.print("Attack: ");
+		int attack = userJunk.nextInt();
+		System.out.print("Defense: ");
+		int defense = userJunk.nextInt();
+		System.out.print("Base: ");
+		int base = userJunk.nextInt();
+		System.out.print("STAB: ");
+		int stab = userJunk.nextInt();
+		System.out.print("HP: ");
+		int hp = userJunk.nextInt();
+		
+		double modifier = 0.85 + (Math.random() * 0.15);		//do calculations
+		double damage = (((2 * level + 10) / (250)) + ((attack) / (defense)) * base + 2) * modifier;
+		
+		System.out.println("\n" + name + " sustained " + (int) damage + " points of damage!"); // print damage and resulting hp
+		System.out.println("HP, after damage, is " + (int)(hp - damage));
+		
+		statsTable(name, hp - damage, level, attack, defense, base, stab);
+	}
+	
+	public static void statsTable(String name, double newhp, int level, int attack, int defense, int base, int stab) {
+		System.out.println("Name		---+---		" + name);
+		System.out.println("Level		---+---		" + level);
+		System.out.println("------------------------------------------");
+		System.out.println("HP		---+---		" + (int)newhp);
+		System.out.println("Attack		---+---		" + attack);
+		System.out.println("Defense		---+---		" + defense);
+		System.out.println("SP. ATK		---+---		121");	//I don't know what these values are,
+		System.out.println("SP. DEF		---+---		81");	//so I am just using the ones in the example
+		System.out.println("Speed		---+---		107");  //Also, this value was never asked for, using the default
+		
 	}
 
 }
